f40df831d6f9e27babc3be7f7fdd8a1679b0bb6d
mohamed-ayman111 <aymanhassanmohamed2@gmail.com>
2026-07-07 05:18:05 -0700

    p9.html / ai/p1,py

--- AI/p1.py
+++ AI/p1.py
@@ -18,8 +18,8 @@ for i in range(3):
                 if grid[x][y]=='G':
                     print("Goal Found")
                     break
-                    for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-                        nx,ny=x+dx,y+dy
+                for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    nx,ny=x+dx,y+dy
                         if 0<= nx <3 and 0 <=ny <3:
                             if grid[nx][ny]!='1' and (nx,ny) not in visited:
                                 queue.append((nx,ny))
